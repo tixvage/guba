f81f9c9bd6284ac97731c5f265941e2d87ae47aa
tixvage <mel10h3421@gmail.com>
2022-12-09 21:11:23 +0300

    added new feature 'delete line'

--- src/settings.zig
+++ src/settings.zig
@@ -8,6 +8,7 @@ pub const keybindings = [_]Keybinding{
     .{ .main_key = c.SDL_SCANCODE_E, .handler = goEol },
     .{ .main_key = c.SDL_SCANCODE_S, .handler = Buffer.save },
     .{ .mod_key = null, .main_key = c.SDL_SCANCODE_F11, .handler = toggleFullscreen },
+    .{ .mod_key = c.SDL_SCANCODE_LSHIFT, .main_key = c.SDL_SCANCODE_DELETE, .handler = deleteCurrentLine },
 };
 
 fn goBol(buffer: *Buffer) !void {
@@ -26,4 +27,10 @@ var fullscreen = false;
 fn toggleFullscreen(buffer: *Buffer) !void {
     fullscreen = !fullscreen;
     _ = c.SDL_SetWindowFullscreen(buffer.window, if (fullscreen) c.SDL_WINDOW_FULLSCREEN_DESKTOP else 0);
+}
+
+fn deleteCurrentLine(buffer: *Buffer) !void {
+    const line_number = buffer.getLineNumber();
+    const deleted_line = buffer.file.orderedRemove(line_number);
+    deleted_line.deinit();
 }
