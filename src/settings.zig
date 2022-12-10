5d41c02fbfda74bdeb2a92d04c511bfa15845028
tixvage <mel10h3421@gmail.com>
2022-12-10 12:16:18 +0300

    new feature 'go next word'

--- src/settings.zig
+++ src/settings.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 const c = @import("sdl2");
+const su = @import("string_utils.zig");
+
 const Buffer = @import("buffer.zig");
 const Keybinding = @import("keybinding.zig");
 
@@ -9,6 +11,7 @@ pub const keybindings = [_]Keybinding{
     .{ .main_key = c.SDL_SCANCODE_S, .handler = Buffer.save },
     .{ .mod_key = null, .main_key = c.SDL_SCANCODE_F11, .handler = toggleFullscreen },
     .{ .mod_key = c.SDL_SCANCODE_LSHIFT, .main_key = c.SDL_SCANCODE_DELETE, .handler = deleteCurrentLine },
+    .{ .mod_key = c.SDL_SCANCODE_LCTRL, .main_key = c.SDL_SCANCODE_RIGHT, .handler = goNextWord },
 };
 
 fn goBol(buffer: *Buffer) !void {
@@ -34,4 +37,27 @@ fn deleteCurrentLine(buffer: *Buffer) !void {
     const deleted_line = buffer.file.orderedRemove(line_number);
     deleted_line.deinit();
     try buffer.tryRecoverHorizontal();
+}
+
+fn goNextWord(buffer: *Buffer) !void {
+    const line = buffer.getCurrentLine();
+    const line_as_unicode = try su.utf8ToUnicode(buffer.allocator, line);
+    const line_len = line_as_unicode.items.len;
+    defer line_as_unicode.deinit();
+
+    const current_pos = buffer.cursor.x;
+    var it = std.mem.split(u32, line_as_unicode.items[@intCast(usize, current_pos)..], &.{' '});
+    if (it.next()) |_| {
+        if (it.index) |index| {
+            buffer.cursor.x = @intCast(i32, index) + current_pos;
+        } else {
+            if (buffer.cursor.x != @intCast(i32, line_len)) {
+                buffer.cursor.x = @intCast(i32, line_len);
+            } else {
+                //next line request
+                std.debug.print("triggered", .{});
+            }
+        }
+    }
+    buffer.saveHorizontal();
 }
